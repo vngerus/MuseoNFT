cd2ef6f98257f5cd08c9a1818bb96c33a82a59b0
angel <angelsmithlgs@gmail.com>
2024-10-24 12:59:15 -0300

    add: imagenes en webp de nft

---
+++ src/assets/nft/index.ts
@@ -0,0 +1,4 @@
+export { default as nft1 } from './nft1.webp'
+export { default as nft2 } from './nft2.webp'
+export { default as nft3 } from './nft3.webp'
+export { default as nft4 } from './nft4.webp'
